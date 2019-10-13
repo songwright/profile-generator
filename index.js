372d1eb2c331208b8cbb3835dfd72bc4a7769cb0
Ramon Sanchez <songwright66@gmail.com>
2019-10-13 15:23:32 -0700

    Updated index.js with variables and writeFile

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
+// Required
+
+var generator = require("./generateHTML.js")
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+
+// Variables and constants
+
+let gitUsername = '';
+let colorTheme = '';
+let imageLink = '';
+let fullName ='';
+let company = '';
+let location = '';
+let profileLink = '';
+let blogLink = '';
+let bioLink = '';
+let repos = 0;
+let followers = 0;
+let starsTotal = 0;
+let following = 0;
+let userData = [];
 const questions = [
-  
+  {
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "username"
+  },
+  {
+    type: "list",
+    message:  "Which color theme would you like?",
+    name: "color",
+    choices: ['green', 'blue', 'pink','red']
+  }
 ];
 
 function writeToFile(fileName, data) {
- 
+  fs.writeFile("index.html", data, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("Success!");
+
+  });
 }
 
 function init() {
